c835b7ad1e514b78845d99a21f1c5e370c74e00d
Wasim2708 <wasim.27a@gmail.com>
2025-08-21 09:47:41 +0530

    Updating ZDLayoutKit to 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,1 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZDLayoutKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZDLayoutKit",
+            targets: ["ZDLayoutKit", "ZDLayoutKitPackage"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/zoho/ZDAttachmentUploader.git", exact: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "ZDLayoutKit", url: "https://maven.zohodl.com/ZohoDesk/ZDLayoutKit/1.0.0/ZDLayoutKit.zip", checksum: "c5e54e7a22316f6fa5db6dfcbf431d12af00b8034f80e12561e6adf3d95bd282"),
+        
+        .target(
+            name: "ZDLayoutKitPackage",
+            dependencies: [
+                .product(name: "ZDAttachmentUploader", package: "ZDAttachmentUploader")
+            ]
+        )
+    ]
+)
 
